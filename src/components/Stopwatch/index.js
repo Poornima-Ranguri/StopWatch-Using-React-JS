5bd91696f6156a11f93c86547d6f77b05a72f3b4
Poornima-Ranguri <poornimaranguri@gmail.com>
2024-08-20 13:38:33 +0000

    Stop Watch using React Js annd Component Life Cycle

--- src/components/Stopwatch/index.js
+++ src/components/Stopwatch/index.js
@@ -2,121 +2,115 @@ import {Component} from 'react'
 
 import './index.css'
 
-const initialState = {
-  isTimerRunning: false,
-  timeElapsedInSeconds: 0,
-  timeInMinutes: 0,
-}
-
 class Stopwatch extends Component {
-  state = initialState
+  state = {
+    isTimerRunning: false,
+    timeElapsedInSeconds: 0,
+  }
 
-  clearTimerInterval = () => clearInterval(this.intervalId)
+  componentWillUnmount() {
+    clearInterval(this.timeInterval)
+  }
 
   onResetTimer = () => {
-    this.clearTimerInterval()
-    this.setState(initialState)
+    clearInterval(this.timeInterval)
+    this.setState({isTimerRunning: false, timeElapsedInSeconds: 0})
   }
 
-  onIncreasingMinutes = () => {
-    const {timeElapsedInSeconds} = this.state
-
-    if (timeElapsedInSeconds === 60) {
-      this.setState(prevState => ({timeInMinutes: prevState.timeInMinutes + 1}))
-    }
+  onStopTimer = () => {
+    clearInterval(this.timeInterval)
+    this.setState({isTimerRunning: false})
   }
 
-  incrementTimeElapsedInSeconds = () => {
-    const {isTimerRunning} = this.state
-
-    if (isTimerRunning) {
-      this.setState(prevState => ({
-        timeElapsedInSeconds: prevState.timeElapsedInSeconds + 1,
-      }))
-    }
+  updateTime = () => {
+    this.setState(prevState => ({
+      timeElapsedInSeconds: prevState.timeElapsedInSeconds + 1,
+    }))
   }
 
   onStartTimer = () => {
-    const {isTimerRunning} = this.state
-
-    if (!isTimerRunning) {
-      this.intervalId = setInterval(this.incrementTimeElapsedInSeconds, 1000)
-    }
-
-    this.setState({isTimerRunning: true}, this.onIncreasingMinutes)
+    this.timeInterval = setInterval(this.updateTime, 1000)
+    this.setState({isTimerRunning: true})
   }
 
-  getTimeInStringFormat = () => {
+  renderSeconds = () => {
+    const {timeElapsedInSeconds} = this.state
+    const seconds = Math.floor(timeElapsedInSeconds % 60)
+
+    if (seconds < 10) {
+      return `0${seconds}`
+    }
+    return seconds
+  }
+
+  renderMinutes = () => {
+    const {timeElapsedInSeconds} = this.state
+    const minutes = Math.floor(timeElapsedInSeconds / 60)
+
+    if (minutes < 10) {
+      return `0${minutes}`
+    }
+    return minutes
+  }
+
+  getStrigifiedElapsedSecondsAndMinutes = () => {
     const {timeElapsedInSeconds} = this.state
 
-    let updatedSeconds = timeElapsedInSeconds % 60 // Get the remainder when divided by 60
-    let updatedMinutes = Math.floor(timeElapsedInSeconds / 60) // Get the quotient when divided by 60
+    const minutes = Math.floor(timeElapsedInSeconds / 60)
+    const seconds = Math.floor(timeElapsedInSeconds % 60)
 
-    // If seconds are 60, reset them to 0 and increment the minutes
-    if (updatedSeconds === 60) {
-      updatedSeconds = 0
-      updatedMinutes += 1
-    }
+    console.log(`elapsed ${timeElapsedInSeconds}`)
+    console.log(seconds)
+    console.log(minutes)
 
-    const stringifiedMinutes =
-      updatedMinutes > 9 ? updatedMinutes : `0${updatedMinutes}`
-    const stringifiedSeconds =
-      updatedSeconds > 9 ? updatedSeconds : `0${updatedSeconds}`
+    const minutesResult = minutes > 9 ? minutes : `0${minutes}`
+    const secondsResult = seconds > 9 ? seconds : `0${seconds}`
 
-    return `${stringifiedMinutes}:${stringifiedSeconds}`
-  }
-
-  onClickingStartButton = () => {
-    this.setState(prevState => ({isTimerRunning: !prevState.isTimerRunning}))
-    this.onStartTimer()
-  }
-
-  stopClock = () => {
-    const {timeInMinutes, timeElapsedInSeconds} = this.state
-    this.clearTimerInterval()
-    this.setState({
-      timeElapsedInSeconds,
-      timeInMinutes,
-      isTimerRunning: false,
-    })
+    return `${minutesResult}:${secondsResult}`
   }
 
   render() {
     const {isTimerRunning} = this.state
-    console.log(isTimerRunning)
+    const time = this.getStrigifiedElapsedSecondsAndMinutes()
 
     return (
       <div className="app-container">
-        <h1 className="heading">Stopwatch</h1>
-
-        <div className="timer-container">
-          <div className="icon-text-container">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/stopwatch-timer.png"
-              alt="stopwatch"
-              className="watch-image"
-            />
-            <p className="parag">Timer</p>
-          </div>
-          <h1 className="parag-seconds">{this.getTimeInStringFormat()}</h1>
-          <div className="buttons-container">
-            <button
-              type="button"
-              className="start-btn"
-              onClick={this.onClickingStartButton}
-            >
-              Start
-            </button>
-            <button type="button" className="stop-btn" onClick={this.stopClock}>
-              Stop
-            </button>
-            <button
-              type="button"
-              className="reset-btn"
-              onClick={this.onResetTimer}
-            >
-              Reset
-            </button>
+        <div className="stopwatch-container">
+          <h1 className="stopwatch">Stopwatch</h1>
+          <div className="timer-container">
+            <div className="timer">
+              <img
+                className="timer-image"
+                src="https://assets.ccbp.in/frontend/react-js/stopwatch-timer.png"
+                alt="stopwatch"
+              />
+              <p className="heading">Timer</p>
+            </div>
+            <h1 className="stopwatch-timer">{time}</h1>
+            <div className="timer-buttons">
+              <button
+                type="button"
+                className="start-button button"
+                onClick={this.onStartTimer}
+                disabled={isTimerRunning}
+              >
+                Start
+              </button>
+              <button
+                type="button"
+                className="stop-button button"
+                onClick={this.onStopTimer}
+              >
+                Stop
+              </button>
+              <button
+                type="button"
+                className="reset-button button"
+                onClick={this.onResetTimer}
+              >
+                Reset
+              </button>
+            </div>
           </div>
         </div>
       </div>
